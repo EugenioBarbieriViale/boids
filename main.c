ea1b1e554d871e805d519e360423b598bd3553c5
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-08-26 14:38:16 +0200

    new

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define W 1000
 #define H 800
 
-#define N 10
+#define N 1
 #define size 30
 
 typedef struct {
@@ -33,21 +33,24 @@ Vector2 rotate(Vector2 pos, float a) {
     return (Vector2){new_x, new_y};
 }
 
-
 void draw_flock(Boid flock[]) {
     for (int i=0; i<N; i++) {
         Vector2 pos = flock[i].pos;
-        float a = flock[i].angle;
+        /* float a = flock[i].angle; */
+        float a = PI/6;
 
-        /* Vector2 v1 = (Vector2){pos.x + size, pos.y}; */
-        /* Vector2 v2 = (Vector2){pos.x, pos.y - (int)(size/2)}; */
-        /* Vector2 v3 = (Vector2){pos.x, pos.y + (int)(size/2)}; */
+        Vector2 d1 = rotate((Vector2){-size, size}, a);
+        Vector2 d2 = rotate((Vector2){size, size}, a);
+        Vector2 d3 = rotate((Vector2){size, -size}, a);
 
-        Vector2 v1 = rotate((Vector2){pos.x - size, pos.y}, a);
-        Vector2 v2 = rotate((Vector2){pos.x, pos.y + (int)(size/2)}, a);
-        Vector2 v3 = rotate((Vector2){pos.x, pos.y - (int)(size/2)}, a);
+        Vector2 v1 = (Vector2){pos.x + d1.x, pos.y + d1.y};
+        Vector2 v2 = (Vector2){pos.x + d2.x, pos.y + d2.y};
+        Vector2 v3 = (Vector2){pos.x + d3.x, pos.y + d3.y};
 
         DrawTriangle(v1, v2, v3, RED);
+        DrawRectangleV(v1, (Vector2){5,5}, YELLOW);
+        /* DrawRectangleV(v2, (Vector2){5,5}, BLUE); */
+        /* DrawRectangleV(v3, (Vector2){5,5}, GREEN); */
     }
 }
 
