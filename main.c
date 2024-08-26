79668b0c1b7eeee3ac5e822706ae97320d066105
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-08-26 13:45:20 +0200

    new

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <raylib.h>
 
 #define W 1000
 #define H 800
 
-#define N 1
+#define N 10
 #define size 30
 
 typedef struct {
@@ -15,17 +16,20 @@ typedef struct {
 } Boid;
 
 void init_flock(Boid flock[]) {
+    float angles[10];
+    for (int i=0; i<N; i++) {
+        angles[i] = (float)i/20;
+    }
+
     for (int i=0; i<N; i++) {
         flock[i].pos = (Vector2){i*30+200, 300};
-        flock[i].angle = PI/6;
+        flock[i].angle = angles[rand() % N];
     }
 }
 
 Vector2 rotate(Vector2 pos, float a) {
-    /* float new_x = (pos.x * size * cos(a) - pos.y * size * sin(a)); */
-    /* float new_y = (pos.x * size * sin(a) + pos.y * size * cos(a)); */
-    float new_x = (pos.x + pos.x * cos(a) - pos.y * sin(a));
-    float new_y = (pos.y + pos.x * sin(a) + pos.y * cos(a));
+    float new_x = (pos.x * cos(a) - pos.y * sin(a));
+    float new_y = (pos.x * sin(a) + pos.y * cos(a));
     return (Vector2){new_x, new_y};
 }
 
@@ -40,8 +44,8 @@ void draw_flock(Boid flock[]) {
         /* Vector2 v3 = (Vector2){pos.x, pos.y + (int)(size/2)}; */
 
         Vector2 v1 = rotate((Vector2){pos.x - size, pos.y}, a);
-        Vector2 v2 = rotate((Vector2){pos.x, pos.y - (int)(size/2)}, a);
-        Vector2 v3 = rotate((Vector2){pos.x, pos.y + (int)(size/2)}, a);
+        Vector2 v2 = rotate((Vector2){pos.x, pos.y + (int)(size/2)}, a);
+        Vector2 v3 = rotate((Vector2){pos.x, pos.y - (int)(size/2)}, a);
 
         DrawTriangle(v1, v2, v3, RED);
     }
