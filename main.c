9522230031161697fd16abbe0d7beb8b474fdbde
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-08-27 16:59:16 +0200

    new

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define W 1000
 #define H 800
 
-#define N 30
+#define N 15
 #define size 30
 
 #define square(x) (x)*(x)
@@ -32,20 +32,40 @@ Vector2 move(Vector2 pos, Vector2 vel) {
     return pos;
 }
 
+float magnitude(Vector2 v) {
+    return sqrt(square(v.x) + square(v.y));
+}
+
+Vector2 update_vel(float angle, float v_magn) {
+    return (Vector2){v_magn*sin(angle), v_magn*cos(angle)};
+}
+
 void init_flock(Boid flock[]) {
     for (int i=0; i<N; i++) {
         flock[i].pos = (Vector2){rand() % W, rand() % H};
         flock[i].angle = rand_angle();
-        float v_magn = rand() % 4+0.4;
-        flock[i].vel = (Vector2){v_magn*sin(flock[i].angle), v_magn*cos(flock[i].angle)};
+
+        float v_magn = rand() % 4 + 1.f;
+        flock[i].vel = update_vel(flock[i].angle, v_magn);
     }
 }
 
-void separation(Boid b, Boid flock[]) {
+void separation(int i, Boid flock[]) {
+}
+
+void alignment(int i, Boid flock[]) {
+}
+
+void cohesion(int i, Boid flock[]) {
+}
+
+void rules(int index, Boid flock[]) {
     for (int i=0; i<N; i++) {
         float d = distance(b.pos, flock[i].pos);
-        if (d <= 50) {
-            printf("%f\n", d);
+        if (d <= 50 && b.pos.x != flock[i].pos.x && b.pos.y != flock[i].pos.y) {
+            separation(index, flock);
+            alignment(index, flock);
+            cohesion(index, flock);
         }
     }
 }
@@ -54,16 +74,17 @@ void draw_flock(Boid flock[]) {
     for (int i=0; i<N; i++) {
         Vector2 pos = flock[i].pos;
         Vector2 vel = flock[i].vel;
-        float a = flock[i].angle-PI/2;
+        float a = flock[i].angle - PI/2;
+
+        /* flock[i].angle += 0.01; */
+        flock[i].vel = update_vel(flock[i].angle, magnitude(vel));
+        flock[i].pos = move(pos, vel);
 
         Vector2 v1 = (Vector2){pos.x + size*cos(a), pos.y - size*sin(a)};
-        Vector2 v2 = (Vector2){pos.x - size*sin(a)/2, pos.y - size*cos(a)/2};
-        Vector2 v3 = (Vector2){pos.x + size*sin(a)/2, pos.y + size*cos(a)/2};
+        Vector2 v2 = (Vector2){pos.x - size*sin(a)/3, pos.y - size*cos(a)/3};
+        Vector2 v3 = (Vector2){pos.x + size*sin(a)/3, pos.y + size*cos(a)/3};
 
-        /* DrawCircleV(pos, 50, YELLOW); */
         DrawTriangle(v1, v2, v3, GRAY);
-
-        flock[i].pos = move(pos, vel);
     }
 }
 
@@ -81,7 +102,7 @@ int main() {
         ClearBackground(BLACK);
 
         for (int i=0; i<N; i++) {
-            separation(flock[i], flock);
+            rules(i, flock);
         }
         draw_flock(flock);
 
